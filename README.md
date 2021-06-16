ec7e1c55a07105b1d58c755abacd79a903e0730f
rmelick-vida <russell@vida.com>
2021-06-15 17:09:59 -0700

    add example SerializerMethodField and readme

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ https://www.django-rest-framework.org/tutorial/quickstart/
 
 to troubleshoot the bug
 https://github.com/tfranzel/drf-spectacular/issues/422
+
+If you deploy with `python manage.py runserver`, you can test by running
+```
+curl -H 'Accept: application/json; indent=4' -u admin:password http://127.0.0.1:8000/users/
+```
--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -5,7 +5,14 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email', 'groups', 'object_types']
+
+    object_types = serializers.SerializerMethodField(read_only=True, default=[])
+
+    def get_object_types(self, obj):
+        return ['<%s: %s>' % (obj.__class__.__name__, obj)]
+
+
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
